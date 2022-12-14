446a9b37ccb6be4aa413df9c2396851fbf400f02
David Sampson <dcsampsonlaptop@gmail.com>
2022-12-14 09:30:03 +0000

    Day 12 Part 1; not yet run to completion

---
+++ 2022/Day_12/day12_part1.py
@@ -0,0 +1,92 @@
+shortest_len = None
+
+def print_map(map: list):
+  return "\n".join(["".join(line) for line in map])
+
+def print_path(path: list, map: list):
+  size = get_map_size(map)
+  grid = [["." for _ in range(size[0])] for _ in range(size[1])]
+
+  for i in range(len(path)):
+    step = path[i]
+    if i + 1 == len(path):
+      grid[step[1]][step[0]] = "E"
+    else:
+      next_step = path[i+1]
+      if next_step[0] > step[0]:
+        grid[step[1]][step[0]] = ">"
+      elif next_step[0] < step[0]:
+        grid[step[1]][step[0]] = "<"
+
+      if next_step[1] > step[1]:
+        grid[step[1]][step[0]] = "v"
+      elif next_step[1] < step[1]:
+        grid[step[1]][step[0]] = "^"
+  
+  return print_map(grid)
+
+def get_map_size(map: list):
+  return (len(map[0]), len(map))
+
+def get_start(map: list):
+  for k in range(len(map)):
+    for i in range(len(map[k])):
+      if map[k][i] == "S":
+        return (i, k)
+  return (0,0)
+
+
+def explore(map:list, pos: tuple, history: list):
+  global shortest_len
+  history_list = []
+  height = map[pos[1]][pos[0]] if map[pos[1]][pos[0]] != "S" else "a"
+  map_size = get_map_size(map)
+  new_history = history.copy()
+  new_history.append(pos)
+
+  if shortest_len and len(new_history) > shortest_len:
+    return history_list
+
+  if height == "E":
+    print(f"FOUND E AT {new_history}")
+    if shortest_len is None or len(new_history) < shortest_len:
+      shortest_len = len(new_history)
+    history_list.append(new_history)
+    return history_list
+
+  north_pos = (pos[0], pos[1] - 1) if pos[1] - 1 >= 0 and (pos[0], pos[1] - 1) not in new_history else None
+  east_pos = (pos[0] + 1, pos[1]) if pos[0] + 1 < map_size[0] and (pos[0] + 1, pos[1]) not in new_history else None
+  south_pos = (pos[0], pos[1] + 1) if pos[1] + 1 < map_size[1] and (pos[0], pos[1] + 1) not in new_history else None
+  west_pos = (pos[0] - 1, pos[1]) if pos[0] - 1 >= 0 and (pos[0] - 1, pos[1]) not in new_history else None
+
+  north_height = (map[north_pos[1]][north_pos[0]] if map[north_pos[1]][north_pos[0]] != "E" else "z") if north_pos else None
+  east_height = (map[east_pos[1]][east_pos[0]] if map[east_pos[1]][east_pos[0]] != "E" else "z") if east_pos else None
+  south_height = (map[south_pos[1]][south_pos[0]] if map[south_pos[1]][south_pos[0]] != "E" else "z") if south_pos else None
+  west_height = (map[west_pos[1]][west_pos[0]] if map[west_pos[1]][west_pos[0]] != "E" else "z") if west_pos else None
+
+  if north_pos and abs(ord(height) - ord(north_height)) <= 1:
+    history_list += explore(map, north_pos, new_history)
+  if east_pos and abs(ord(height) - ord(east_height)) <= 1:
+    history_list += explore(map, east_pos, new_history)
+  if south_pos and abs(ord(height) - ord(south_height)) <= 1:
+    history_list += explore(map, south_pos, new_history)
+  if west_pos and abs(ord(height) - ord(west_height)) <= 1:
+    history_list += explore(map, west_pos, new_history)
+
+  return history_list
+
+  
+  
+
+input_file = open('input.txt', 'r')
+map = [list(line) for line in input_file.read().rstrip().split('\n')]
+# print(print_map(map))
+
+start_pos = get_start(map)
+
+history_list = explore(map, start_pos, [])
+print([len(history[1:]) for history in history_list])
+
+path = history_list[0]
+
+print(print_path(path, map))
