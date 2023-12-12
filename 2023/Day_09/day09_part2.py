1418c86f46ad867bc1dff0b95c377e90a0e986ae
David Sampson <david.sampson@mobilise.cloud>
2023-12-12 11:15:09 +0000

    Day 9 part 1 & 2

---
+++ 2023/Day_09/day09_part2.py
@@ -0,0 +1,19 @@
+file_name = "input.txt"
+
+def continue_sequence(sequence: list) -> list:
+  new_sequence = sequence.copy()
+  if all([num == 0 for num in sequence]):
+    new_sequence = [0] + new_sequence + [0]
+    return new_sequence
+  
+  differences = [sequence[i] - sequence[i-1] for i in range(1, len(sequence))]
+  differences_continued = continue_sequence(differences)
+  new_sequence = [new_sequence[0] - differences_continued[0]] + new_sequence + [new_sequence[-1] + differences_continued[-1]]
+
+  return new_sequence
+
+input_file = open(file_name, 'r')
+sequences = [[int(num) for num in line.split(" ")] for line in input_file.read().rstrip().split('\n')]
+continued_sequences = [continue_sequence(sequence) for sequence in sequences]
+
+print(f"The sum of all next steps in each sequence is {sum([sequence[0] for sequence in continued_sequences])}")
