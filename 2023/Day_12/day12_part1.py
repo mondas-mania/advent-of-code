17785f49b6ea86b9859f22dfa9864638ccea4015
David Sampson <david.sampson@mobilise.cloud>
2023-12-14 11:40:43 +0000

    Day 12 part 1 (highly inefficient)

---
+++ 2023/Day_12/day12_part1.py
@@ -0,0 +1,51 @@
+file_name = "input.txt"
+
+def get_contiguous_count(springs: list, pos: int) -> int:
+  cut_list = springs[pos:]
+  counter = 0
+  for i in range(len(cut_list)):
+    if cut_list[i] != cut_list[0]:
+      return counter
+    counter += 1
+  return counter
+
+def get_contiguous_list(springs: list) -> list:
+  contiguous_counts = []
+  pos = 0
+  while pos < len(springs):
+    if springs[pos] in [".", "?"]:
+      pos += 1
+      continue
+    else:
+      count = get_contiguous_count(springs, pos)
+      contiguous_counts.append(count)
+      pos += count
+  return contiguous_counts
+
+def evaluate_configuration(springs: list, target: list) -> bool:
+  # Evaluate if a list of springs is valid against the target
+  contiguous_counts = get_contiguous_list(springs)
+  evaluation = contiguous_counts == target
+  return evaluation
+
+def get_spring_permuation(springs: list) -> list:
+  if "?" not in springs:
+    return [springs]
+  
+  spring_permutations = []
+  for potential in [".", "#"]:
+    new_springs = springs.copy()
+    pos = new_springs.index("?")
+    new_springs[pos] = potential
+    spring_permutations += get_spring_permuation(new_springs)
+  
+  return spring_permutations
+
+
+input_file = open(file_name, 'r')
+spring_values = [[list(line.split(" ")[0]), [int(num) for num in line.split(" ")[1].split(",")]] for line in input_file.read().rstrip().split('\n')]
+
+spring_permutations = [get_spring_permuation(values[0]) for values in spring_values]
+valid_permutations = [[permutation for permutation in spring_permutations[i] if evaluate_configuration(permutation, spring_values[i][1])] for i in range(len(spring_values))]
+print(f"There are {sum([len(permutations) for permutations in valid_permutations])} possible arrangements in total.")
+None
