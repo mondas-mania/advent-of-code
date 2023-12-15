d44b76f56064918a33eaebce562c1cef5c580fa8
David Sampson <david.sampson@mobilise.cloud>
2023-12-15 12:23:20 +0000

    Day 15 part 1

---
+++ 2023/Day_15/day15_part1.py
@@ -0,0 +1,19 @@
+file_name = "input.txt"
+
+def get_hash(hash: str) -> int:
+  current_value = 0
+  for char in list(hash):
+    current_value = current_value + ord(char)
+    current_value = current_value * 17
+    current_value = current_value % 256
+  return current_value
+
+input_file = open(file_name, 'r')
+init_seq = (input_file.read().rstrip().split('\n')[0]).split(",")
+
+hash_values = [get_hash(seq) for seq in init_seq]
+total = sum(hash_values)
+
+print(f"The sum of all the hashes is {total}")
+
+None
