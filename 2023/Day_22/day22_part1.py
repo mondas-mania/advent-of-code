1f1565be3ca484b84eea0c4cbf7f9af37fd3e976
David Sampson <david.sampson@mobilise.cloud>
2024-01-03 11:58:52 +0000

    Settling bricks for day 22 (2023)

--- 2023/Day_22/day22_part1.py
+++ 2023/Day_22/day22_part1.py
@@ -1,4 +1,4 @@
-file_name = "input.txt"
+file_name = "input_test.txt"
 
 
 def get_highest_values(bricks: list) -> tuple:
@@ -46,24 +46,66 @@ def plot_bricks(bricks: list, axis_hor: str, axis_ver: str = "z") -> str:
     for pos in brick:
       pos_hor = pos[ax_hor]
       pos_ver = pos[ax_ver]
-      grid[pos_ver][pos_hor] = chr(i + 65)
+      grid[pos_ver][pos_hor] = chr(i + 65) if grid[pos_ver][pos_hor] == "." or grid[pos_ver][pos_hor] == chr(i + 65) else "?"
 
   return "\n".join(["".join(row) for row in grid[::-1]])
+
+
+def get_supporting_bricks(brick: list, bricks: list) -> list:
+  supporting_bricks = []
+  brick_positions = get_brick_cells(brick)
+
+  for other_brick in bricks:
+    if other_brick == brick:
+      continue
+    other_brick_positions = get_brick_cells(other_brick)
+    for pos in brick_positions:
+      if (pos[0], pos[1], pos[2] - 1) in other_brick_positions:
+        supporting_bricks.append(other_brick)
+        break
+  return supporting_bricks
+
+
+def is_on_ground(brick: list) -> bool:
+  brick_positions = get_brick_cells(brick)
+  for pos in brick_positions:
+    if pos[2] == 1:
+      return True
+  return False
+
+
+def settle_bricks(bricks: list) -> list:
+  to_settle = True
+  settled_bricks = bricks.copy()
+  while to_settle:
+    to_settle = False
+    settling_bricks = []
+    for brick in settled_bricks:
+      supporting_bricks = get_supporting_bricks(brick, settled_bricks)
+      if len(supporting_bricks) > 0 or is_on_ground(brick):
+        settling_bricks.append(brick)
+      else:
+        to_settle = True
+        settling_bricks.append([(brick[0][0], brick[0][1], brick[0][2] - 1), (brick[1][0], brick[1][1], brick[1][2] - 1)])
+
+    settled_bricks = settling_bricks.copy()
+  return settled_bricks
 
 
 input_file = open(file_name, 'r')
 bricks = [[tuple([int(pos) for pos in coords.split(",")]) for coords in line.split("~")] for line in input_file.read().rstrip().split('\n')]
 
 sorted_bricks = sort_bricks(bricks)
+settled_bricks = settle_bricks(bricks)
 
-string_bricks_x = plot_bricks(sorted_bricks[0:26], "x")
-string_bricks_y = plot_bricks(sorted_bricks[0:26], "y")
-string_bricks_x_y = plot_bricks(sorted_bricks[0:26], "x", "y")
+string_bricks_x = plot_bricks(settled_bricks[0:26], "x")
+string_bricks_y = plot_bricks(settled_bricks[0:26], "y")
+string_bricks_x_y = plot_bricks(settled_bricks[0:26], "x", "y")
 print()
 print(string_bricks_x)
 print()
 print(string_bricks_y)
 print()
-print(string_bricks_x_y)
+# print(string_bricks_x_y)
 
 None
